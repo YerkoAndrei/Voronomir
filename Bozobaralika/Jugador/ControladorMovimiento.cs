8de4423cc006467e4df91b545b4a830bc11bf112
Yerko Andrei <yerkoandrei@gmail.com>
2024-03-05 21:13:42 -0300

    movimiento y salto

--- Bozobaralika/Jugador/ControladorMovimiento.cs
+++ Bozobaralika/Jugador/ControladorMovimiento.cs
@@ -9,22 +9,37 @@ public class ControladorMovimiento : SyncScript
 {
     public CharacterComponent cuerpo;
     public CameraComponent cámara;
+    public float sensibilidad;
 
     private Vector3 movimiento;
     private float multiplicadorVelocidad;
     private float aceleración;
 
+    private float rotaciónX;
+    private float rotaciónY;
+
     public override void Start()
     {
         multiplicadorVelocidad = 4;
+
+        // interfaz
+        Input.LockMousePosition(true);
+        Game.IsMouseVisible = false;
     }
 
     // PENDIENTE: Control
     public override void Update()
     {
+        // interfaz
+        if (Input.IsKeyPressed(Keys.Space))
+        {
+            Input.LockMousePosition(false);
+            Game.IsMouseVisible = true;
+        }
+
         // Correr
-        if (cuerpo.IsGrounded)
-            Correr();
+        Mirar();
+        Correr();
 
         // Salto
         if (Input.IsKeyPressed(Keys.Space))
@@ -41,16 +56,17 @@ public class ControladorMovimiento : SyncScript
 
     private void Correr()
     {
+        // PENDIENTE: controlar cambios bruscos
         movimiento = new Vector3();
 
         if (Input.IsKeyDown(Keys.W) || Input.IsKeyDown(Keys.Up))
-            movimiento += -Vector3.UnitZ;
+            movimiento -= Vector3.UnitZ;
         if (Input.IsKeyDown(Keys.A) || Input.IsKeyDown(Keys.Left))
-            movimiento += -Vector3.UnitX;
+            movimiento -= Vector3.UnitX;
         if (Input.IsKeyDown(Keys.S) || Input.IsKeyDown(Keys.Down))
-            movimiento += +Vector3.UnitZ;
+            movimiento += Vector3.UnitZ;
         if (Input.IsKeyDown(Keys.D) || Input.IsKeyDown(Keys.Right))
-            movimiento += +Vector3.UnitX;
+            movimiento += Vector3.UnitX;
 
         // Aceleración
         if (movimiento == Vector3.Zero)
@@ -61,8 +77,21 @@ public class ControladorMovimiento : SyncScript
             aceleración = MathUtil.Clamp(aceleración, 1, 2);
         }
 
+        // Movimiento
+        movimiento = Vector3.Transform(movimiento, cámara.Entity.Transform.Rotation);
+        movimiento.Y = 0;
+
         movimiento.Normalize();
         cuerpo.SetVelocity(movimiento * aceleración * multiplicadorVelocidad);
+    }
+
+    private void Mirar()
+    {
+        rotaciónX -= Input.MouseDelta.X * sensibilidad;
+        rotaciónY -= Input.MouseDelta.Y * sensibilidad;
+        rotaciónY = MathUtil.Clamp(rotaciónY, -MathUtil.PiOverTwo, MathUtil.PiOverTwo);
+
+        cámara.Entity.Transform.Rotation = Quaternion.RotationYawPitchRoll(rotaciónX, rotaciónY, 0);
     }
 
     private void Saltar()
