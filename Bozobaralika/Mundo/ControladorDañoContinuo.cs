6368b9a5f650e21c08cfe7b67a2592d797f27a31
Yerko Andrei <yerkoandrei@gmail.com>
2024-03-27 22:06:24 -0300

    lava y daño continuo

---
+++ Bozobaralika/Mundo/ControladorDañoContinuo.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Stride.Engine;
+using Stride.Physics;
+
+namespace Bozobaralika;
+
+public class ControladorDañoContinuo : AsyncScript
+{
+    private StaticColliderComponent cuerpo;
+    private float daño;
+
+    public override async Task Execute()
+    {
+        cuerpo = Entity.Get<StaticColliderComponent>();
+        daño = 10f;
+
+        while (Game.IsRunning)
+        {
+            await cuerpo.NewCollision();
+            IntentarDañar();
+
+            await Script.NextFrame();
+        }
+    }
+
+    private async void IntentarDañar()
+    {
+        while(cuerpo.Collisions.Count > 0)
+        {
+            Dañar(cuerpo.Collisions.ToArray());
+            await Task.Delay(500);
+        }
+    }
+
+    private void Dañar(Collision[] colisiones)
+    {
+        foreach (var colisión in colisiones)
+        {
+            var jugador = colisión.ColliderA.Entity.Get<ControladorJugador>();
+            if (jugador == null)
+                jugador = colisión.ColliderB.Entity.Get<ControladorJugador>();
+
+            if (jugador == null)
+                continue;
+
+            // PENDIENTE: efectos
+            jugador.RecibirDaño(daño);
+        }
+    }
+}
