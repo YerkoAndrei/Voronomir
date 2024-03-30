b271a296c0a5295a8a5a406adec8ebc7e58d2475
Yerko Andrei <yerkoandrei@gmail.com>
2024-03-29 23:14:28 -0300

    saltadores sin terminar

---
+++ Bozobaralika/Mundo/ControladorSaltador.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.Physics;
+
+namespace Bozobaralika;
+
+public class ControladorSaltador : AsyncScript
+{
+    public float fuerza;
+    private Vector3 dirección;
+
+    public override async Task Execute()
+    {
+        var cuerpo = Entity.Get<StaticColliderComponent>();
+        dirección = new Vector3(0, fuerza, 0);
+
+        while (Game.IsRunning)
+        {
+            await cuerpo.NewCollision();
+            Saltar(cuerpo.Collisions.ToArray());
+
+            await Script.NextFrame();
+        }
+    }
+
+    private void Saltar(Collision[] colisiones)
+    {
+        foreach (var colisión in colisiones)
+        {
+            var cuerpo = colisión.ColliderA.Entity.Get<CharacterComponent>();
+            if (cuerpo == null)
+                cuerpo = colisión.ColliderB.Entity.Get<CharacterComponent>();
+
+            if (cuerpo == null)
+                continue;
+
+            cuerpo.Jump(dirección);
+        }
+    }
+}
