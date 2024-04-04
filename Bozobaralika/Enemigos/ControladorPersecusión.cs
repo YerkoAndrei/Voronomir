dcd4c27ac10743682fffe18fe6ba769af035e763
Yerko Andrei <yerkoandrei@gmail.com>
2024-04-04 18:42:01 -0300

    correccion morir enemigo

--- Bozobaralika/Enemigos/ControladorPersecusión.cs
+++ Bozobaralika/Enemigos/ControladorPersecusión.cs
@@ -205,7 +205,8 @@ public class ControladorPersecusión : StartupScript
 
     public void EliminarPersecutor()
     {
-        persecutor.EliminarPersecutor(this);
+        if (persecutorTrigonométrico)
+            persecutor.EliminarPersecutor(this);
     }
 
     public void ReasignarÍndice(int índice)
